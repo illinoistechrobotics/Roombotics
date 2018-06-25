3381b201768c7ae6008a826123fad720176d33ca
SholessJo3 <gojoericker@comcast.net>
2018-06-25 23:33:42 +0000

    more text updates

--- control.py
+++ control.py
@@ -2,22 +2,23 @@ import pygame
 import serial
 import time #These are the required libraries (make sure they are installed)
 
-def sendPacket(byte):
+def sendPacket(byte): #user defines packet
     packet = [byte] #now we will send this packet over to the arduino
 
-    for i in packet: #make this a function Joe
+    for i in packet: #probably a better way to do this
         elements = [i]
         print( bytes(elements))
-        ser.write(bytes(elements))
-    time.sleep(.01);
+        ser.write(bytes(elements)) #sends packets over xbee
+    time.sleep(.01); #wait a bit between sending
 
     return;
 
 ser = serial.Serial('/dev/ttyUSB0', 57600, timeout=1) #this is the port for your xbee
 
 
-pygame.joystick.init()
-joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
+pygame.joystick.init() 
+joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())] #pygame will find any number of joysticks present on your computer
+
 
 pygame.init()
 pygame.joystick.init()
@@ -26,7 +27,7 @@ Xaxis = 0
 Yaxis = 0
 button = []
 
-for i in range(12):
+for i in range(12):#create an array to put button values in
     button.append(0)
 
 
@@ -41,29 +42,25 @@ while done==False:
         joystick = pygame.joystick.Joystick(i)
         joystick.init()
 
-        for i in range(4):#joystick.get_numbuttons()):
+        for i in range(4): #send the first few values of buttons (adding more and decreasing the delay on sending packets seems to make recieved packets less accurate)
             button[i] = joystick.get_button(i)
             sendPacket(((2)<<5)+button[i]+(i<<1))
             print(button)
             
 
-        #print("Controller is " + str(i) + " " + joystick.get_name())
-
-        #if pygame.joystick.get_count() == 2:
-        #   print("Num joysticks correct")
-
-        #for f in range(joystick.get_numaxes()):
-        #    print("Axis " + str(f) + " position " + str(joystick.get_axis(f))) 
+        
 
         Xaxis = round(joystick.get_axis(0) * 15 + 15) #this should be the "drone" setup for X and Y axes.  The last joystick found is the primary
         Yaxis = round(joystick.get_axis(3)* 15 + 15)
+
+        #The joystick axis are indexed at 0, so Left X = 0, Left Y = 1, Right X = 2, Right Y = 3 and so on if you have more
 
         print("X axis = " + str(Xaxis) + " Y axis = " + str(Yaxis))
 
         packet = [Xaxis, Yaxis + ( 1 << 5)] #now we will send this packet over to the arduino
         sendPacket(Xaxis)
         sendPacket(Yaxis + (1 << 5))
-        time.sleep(.1)
+        
 
 
 
